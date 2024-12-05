ccbf0e5d58b19443a1957ece3d26b696ea28d9de
MaxBTD <97893081+MaxBTD@users.noreply.github.com>
2024-12-05 18:38:59 +0100

    Dodaj pliki projektów.

---
+++ graElfow/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace graElfow
+{
+    internal class Program
+    {
+        static private string zliczCiag(string ciag)
+        {
+            string wynik = "";
+            int count = 0;
+            char sprawChar = ciag[0];
+
+            foreach(char c in ciag){
+                if(c != sprawChar){
+                    wynik += $"{count}{sprawChar}";
+                    sprawChar = c;
+                    count = 0;
+                }
+                count++;
+            }
+            wynik += $"{count}{sprawChar}";
+
+            return wynik;
+        }
+
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int n);
+
+            for(int i = 0; i < n; i++){
+                Console.WriteLine(zliczCiag(Console.ReadLine()));
+            }
+        }
+    }
+}
